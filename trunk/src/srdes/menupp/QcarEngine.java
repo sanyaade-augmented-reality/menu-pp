4706b5885a84b58a26594d0ddcfbe5bb5b30333c
webheadz3@gmail.com <webheadz3@gmail.com@aee4f25e-f0af-d29d-9c76-8bc1b7839efd>
2012-01-25 04:34:03 +0000

    Made a quick patch to get the camera working. Will commit a full fledge structure change to modularize code better.

---
+++ trunk/src/srdes/menupp/QcarEngine.java
@@ -0,0 +1,44 @@
+package srdes.menupp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QcarEngine extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+	}
+
+}
