8cfd6414ae29e6600908828c074f32dd4f818352
webheadz3@gmail.com <webheadz3@gmail.com@aee4f25e-f0af-d29d-9c76-8bc1b7839efd>
2012-01-31 18:54:17 +0000

    Added 2D planes with pictures instead of tea pot.

--- jni/menupp.cpp
+++ jni/menupp.cpp
@@ -29,11 +29,15 @@
 #include <QCAR/QCAR.h>
 #include <QCAR/CameraDevice.h>
 #include <QCAR/Renderer.h>
+#include <QCAR/ImageTarget.h>
+#include <QCAR/VirtualButton.h>
+#include <QCAR/Rectangle.h>
 #include <QCAR/VideoBackgroundConfig.h>
 #include <QCAR/Trackable.h>
 #include <QCAR/Tool.h>
 #include <QCAR/Tracker.h>
 #include <QCAR/CameraCalibration.h>
+#include <QCAR/UpdateCallback.h>
 
 #include "SampleUtils.h"
 #include "Texture.h"
@@ -45,6 +49,70 @@
 extern "C"
 {
 #endif
+
+// Plane Definitions
+// Define a plane to bind an image to..
+static const float planeVertices[] =
+{
+    -0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.5, 0.5, 0.0, -0.5, 0.5, 0.0,
+};
+
+static const float planeTexcoords[] =
+{
+    0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0
+};
+
+static const float planeNormals[] =
+{
+    0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0
+};
+
+static const unsigned short planeIndices[] =
+{
+    0, 1, 2, 0, 2, 3
+};
+
+enum BUTTONS
+{
+    BUTTON_1                    = 1,
+    BUTTON_2                    = 2,
+    BUTTON_3                    = 4,
+    BUTTON_4                    = 8
+};
+
+static const char* lineMeshVertexShader = " \
+  \
+attribute vec4 vertexPosition; \
+ \
+uniform mat4 modelViewProjectionMatrix; \
+ \
+void main() \
+{ \
+   gl_Position = modelViewProjectionMatrix * vertexPosition; \
+} \
+";
+
+
+static const char* lineFragmentShader = " \
+ \
+precision mediump float; \
+ \
+void main() \
+{ \
+   gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0); \
+} \
+";
+
+// OpenGL ES 2.0 specific (Virtual Buttons):
+unsigned int vbShaderProgramID  = 0;
+GLint vbVertexHandle            = 0;
+int buttonMask                  = 0;
+
+// Virtual Button runtime creation:
+bool updateBtns                   = false;
+const char* virtualButtonColors[] = {"red", "blue", "yellow", "green"};
+const int NUM_BUTTONS             = 4;
+
 
 // Textures:
 int textureCount                = 0;
@@ -70,8 +138,8 @@ bool isActivityInPortraitMode   = false;
 QCAR::Matrix44F projectionMatrix;
 
 // Constants:
-static const float kObjectScale = 3.f;
-
+//static const float kObjectScale = 3.f;
+static const float kObjectScale = 200;
 
 JNIEXPORT int JNICALL
 Java_srdes_menupp_QcarEngine_getOpenGlEsVersionNative(JNIEnv *, jobject)
@@ -90,16 +158,127 @@ Java_srdes_menupp_QcarEngine_setActivityPortraitMode(JNIEnv *, jobject, jboolean
     isActivityInPortraitMode = isPortrait;
 }
 
+// Add a button to the list of buttons which are toggled in the next update call
+JNIEXPORT void JNICALL
+Java_srdes_menupp_QcarEngine_addButtonToToggle(JNIEnv */*env*/, jobject /*obj*/, jint virtualButtonIdx)
+{
+    LOG("Java_com_qualcomm_QCARSamples_VirtualButtons_VirtualButtons_addButtonToToggle");
+
+    assert(virtualButtonIdx >= 0 && virtualButtonIdx < NUM_BUTTONS);
+
+    switch (virtualButtonIdx)
+    {
+        case 0:
+            buttonMask |= BUTTON_1;
+            break;
+
+        case 1:
+            buttonMask |= BUTTON_2;
+            break;
+
+        case 2:
+            buttonMask |= BUTTON_3;
+            break;
+
+        case 3:
+            buttonMask |= BUTTON_4;
+            break;
+    }
+    updateBtns = true;
+}
 
 JNIEXPORT void JNICALL
 Java_srdes_menupp_QcarEngine_onQCARInitializedNative(JNIEnv *, jobject)
 {
-    // Comment in to enable tracking of up to 2 targets simultaneously and
-    // split the work over multiple frames:
-    // QCAR::setHint(QCAR::HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS, 2);
-    // QCAR::setHint(QCAR::HINT_IMAGE_TARGET_MULTI_FRAME_ENABLED, 1);
+    LOG("Java_srdes_menupp_QcarEngine_onQCARInitializedNative");
+    QCAR::setHint(QCAR::HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS, 5);
 }
 
+// Create/destroy a Virtual Button at runtime
+//
+// Note: This will NOT work if the tracker is active!
+bool
+toggleVirtualButton(QCAR::ImageTarget* imageTarget, const char* name,
+                    float left, float top, float right, float bottom)
+{
+    LOG("toggleVirtualButton");
+
+    bool buttonToggleSuccess = false;
+
+    QCAR::VirtualButton* virtualButton = imageTarget->getVirtualButton(name);
+    if (virtualButton != NULL)
+    {
+        LOG("Destroying Virtual Button");
+        buttonToggleSuccess = imageTarget->destroyVirtualButton(virtualButton);
+    }
+    else
+    {
+        LOG("Creating Virtual Button");
+        QCAR::Rectangle vbRectangle(left, top, right, bottom);
+        QCAR::VirtualButton* virtualButton = imageTarget->createVirtualButton(name, vbRectangle);
+
+        // This is just a showcase. The values used here a set by default on Virtual Button creation
+        virtualButton->setEnabled(true);
+        virtualButton->setSensitivity(QCAR::VirtualButton::MEDIUM);
+
+        if (virtualButton != NULL)
+            buttonToggleSuccess = true;
+    }
+
+    return buttonToggleSuccess;
+}
+
+
+// Object to receive update callbacks from QCAR SDK
+class VirtualButton_UpdateCallback : public QCAR::UpdateCallback
+{
+    virtual void QCAR_onUpdate(QCAR::State& /*state*/)
+    {
+        if (updateBtns)
+        {
+            // Update runs in the tracking thread therefore it is guaranteed that the tracker is
+            // not doing anything at this point. => Reconfiguration is possible.
+
+            assert(QCAR::Tracker::getInstance().getNumTrackables() > 0);
+            QCAR::Trackable* trackable = QCAR::Tracker::getInstance().getTrackable(0);
+
+            assert(trackable);
+            assert(trackable->getType() == QCAR::Trackable::IMAGE_TARGET);
+            QCAR::ImageTarget* imageTarget = static_cast<QCAR::ImageTarget*>(trackable);
+
+
+            if (buttonMask & BUTTON_1)
+            {
+                LOG("Toggle Button 1");
+
+                toggleVirtualButton(imageTarget, "enchiladas", -180, 180, 180, -180);
+
+            }
+            if (buttonMask & BUTTON_2)
+            {
+                LOG("Toggle Button 2");
+
+                toggleVirtualButton(imageTarget, "hotdog", -180, 180, 180, -180);
+            }
+            if (buttonMask & BUTTON_3)
+            {
+                LOG("Toggle Button 3");
+
+                toggleVirtualButton(imageTarget, "pizza", -180, 180, 180, -180);
+            }
+            if (buttonMask & BUTTON_4)
+            {
+                LOG("Toggle Button 4");
+
+                toggleVirtualButton(imageTarget, virtualButtonColors[3],
+                                    76.57f, -53.52f, 109.50f, -65.87f);
+            }
+
+            buttonMask = 0;
+            updateBtns = false;
+        }
+    }
+} qcarUpdate;
 
 JNIEXPORT void JNICALL
 Java_srdes_menupp_menuppRenderer_renderFrame(JNIEnv *, jobject)
@@ -132,11 +311,40 @@ Java_srdes_menupp_menuppRenderer_renderFrame(JNIEnv *, jobject)
         // Get the trackable:
         const QCAR::Trackable* trackable = state.getActiveTrackable(tIdx);
         QCAR::Matrix44F modelViewMatrix =
-            QCAR::Tool::convertPose2GLMatrix(trackable->getPose());        
+            QCAR::Tool::convertPose2GLMatrix(trackable->getPose());
+
+        // The image target:
+        //assert(trackable->getType() == QCAR::Trackable::IMAGE_TARGET);
+        //const QCAR::ImageTarget* target =
+        //    static_cast<const QCAR::ImageTarget*>(trackable);
+
+        //const QCAR::VirtualButton* button = target->getVirtualButton(0);
+
+
 
         // Choose the texture based on the target name:
-        int textureIndex = (!strcmp(trackable->getName(), "item_1")) ? 0 : 1;
-        const Texture* const thisTexture = textures[textureIndex];
+        Texture* imgTexture;
+
+        if(strcmp(trackable->getName(), "item_1") == 0) {
+        	imgTexture = textures[0];
+        } else if (strcmp(trackable->getName(), "item_2") == 0) {
+        	imgTexture = textures[1];
+        } else if (strcmp(trackable->getName(), "item_3") == 0) {
+        	imgTexture = textures[2];
+        } else if (strcmp(trackable->getName(), "item_4") == 0) {
+        	imgTexture = textures[3];
+        } else if (strcmp(trackable->getName(), "item_5") == 0) {
+        	imgTexture = textures[4];
+        } else continue;
+
+
+        // If the button is pressed, than use this texture:
+        //if (button->isPressed() && strcmp(trackable->getName(), "item_1" == 0))
+        //{
+        	//imgTexture = textures[2];
+        //}
+
+
 
 #ifdef USE_OPENGL_ES_1_1
         // Load projection matrix:
@@ -166,6 +374,7 @@ Java_srdes_menupp_menuppRenderer_renderFrame(JNIEnv *, jobject)
 	//glDrawArrays(GL_TRIANGLES, 0, bananaNumVerts);
 #else
 
+        /*
         QCAR::Matrix44F modelViewProjection;
 
         SampleUtils::translatePoseMatrix(0.0f, 0.0f, kObjectScale,
@@ -202,6 +411,36 @@ Java_srdes_menupp_menuppRenderer_renderFrame(JNIEnv *, jobject)
 	//glDrawArrays(GL_TRIANGLES, 0, bananaNumVerts);
 
         SampleUtils::checkGlError("menupp renderFrame");
+        */
+
+    	// Place an image on the target using a 3D plane
+
+            QCAR::Matrix44F modelViewProjection;
+
+            SampleUtils::translatePoseMatrix(0.0f, 0.0f, kObjectScale,
+                                             &modelViewMatrix.data[0]);
+            SampleUtils::scalePoseMatrix(kObjectScale, kObjectScale, 1.0f,
+                                         &modelViewMatrix.data[0]);
+            SampleUtils::multiplyMatrix(&projectionMatrix.data[0],
+                                        &modelViewMatrix.data[0] ,
+                                        &modelViewProjection.data[0]);
+
+            glUseProgram(shaderProgramID);
+
+            glVertexAttribPointer(vertexHandle, 3, GL_FLOAT, GL_FALSE, 0,
+                                  (const GLvoid*) &planeVertices[0]);
+            glVertexAttribPointer(normalHandle, 3, GL_FLOAT, GL_FALSE, 0,
+                                  (const GLvoid*) &planeNormals[0]);
+            glVertexAttribPointer(textureCoordHandle, 2, GL_FLOAT, GL_FALSE, 0,
+                                  (const GLvoid*) &planeTexcoords[0]);
+
+            glEnableVertexAttribArray(vertexHandle);
+            glEnableVertexAttribArray(normalHandle);
+            glEnableVertexAttribArray(textureCoordHandle);
+
+            glBindTexture(GL_TEXTURE_2D, imgTexture->mTextureID);
+            glUniformMatrix4fv(mvpMatrixHandle, 1, GL_FALSE, (GLfloat*)&modelViewProjection.data[0] );
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, (const GLvoid*) &planeIndices[0]);
 #endif
 
     }
@@ -261,8 +500,7 @@ configureVideoBackground()
 
 
 JNIEXPORT void JNICALL
-Java_srdes_menupp_QcarEngine_initApplicationNative(
-                            JNIEnv* env, jobject obj, jint width, jint height)
+Java_srdes_menupp_QcarEngine_initApplicationNative( JNIEnv* env, jobject obj, jint width, jint height)
 {
     LOG("Java_srdes_menupp_menupp_initApplicationNative");
     
@@ -309,7 +547,7 @@ Java_srdes_menupp_QcarEngine_initApplicationNative(
             LOG("GetTexture() returned zero pointer");
             return;
         }
-
+        LOG("Texture Registered");
         textures[i] = Texture::create(env, textureObject);
     }
 }
@@ -394,19 +632,19 @@ Java_srdes_menupp_QcarEngine_stopCamera(JNIEnv *,
 }
 
 JNIEXPORT jboolean JNICALL
-Java_srdes_menupp_menupp_toggleFlash(JNIEnv*, jobject, jboolean flash)
+Java_srdes_menupp_QcarEngine_toggleFlash(JNIEnv*, jobject, jboolean flash)
 {
     return QCAR::CameraDevice::getInstance().setFlashTorchMode((flash==JNI_TRUE)) ? JNI_TRUE : JNI_FALSE;
 }
 
 JNIEXPORT jboolean JNICALL
-Java_srdes_menupp_menupp_autofocus(JNIEnv*, jobject)
+Java_srdes_menupp_QcarEngine_autofocus(JNIEnv*, jobject)
 {
     return QCAR::CameraDevice::getInstance().startAutoFocus()?JNI_TRUE:JNI_FALSE;
 }
 
 JNIEXPORT jboolean JNICALL
-Java_srdes_menupp_menupp_setFocusMode(JNIEnv*, jobject, jint mode)
+Java_srdes_menupp_QcarEngine_setFocusMode(JNIEnv*, jobject, jint mode)
 {
     return QCAR::CameraDevice::getInstance().setFocusMode(mode)?JNI_TRUE:JNI_FALSE;
 }
@@ -447,6 +685,12 @@ Java_srdes_menupp_menuppRenderer_initRendering(
                                                 "modelViewProjectionMatrix");
 
 #endif
+
+    // OpenGL setup for Virtual Buttons
+    vbShaderProgramID   = SampleUtils::createProgramFromBuffer(lineMeshVertexShader,
+                                                               lineFragmentShader);
+
+    vbVertexHandle      = glGetAttribLocation(vbShaderProgramID, "vertexPosition");
 
 }
 
