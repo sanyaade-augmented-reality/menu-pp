a6fc2963b25e4cdba6b68d296309ff3991858752
jachang116@gmail.com <jachang116@gmail.com@aee4f25e-f0af-d29d-9c76-8bc1b7839efd>
2012-02-05 02:47:03 +0000

    Jason

---
+++ jason/obj/local/armeabi-v7a/objs-debug/menupp/Texture.o.d
@@ -0,0 +1,64 @@
+obj/local/armeabi-v7a/objs-debug/menupp/Texture.o: jni/Texture.cpp \
+ jni/Texture.h \
+ /Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/jni.h \
+ jni/SampleUtils.h \
+ /Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/stdio.h \
+ /Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ /Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/_types.h \
+ /Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/machine/_types.h \
+ /Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/types.h \
+ /Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/stdint.h \
+ /Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/posix_types.h \
+ /Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/stddef.h \
+ /Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/compiler.h \
+ /Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/asm/posix_types.h \
+ /Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/asm/types.h \
+ /Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/types.h \
+ /Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/machine/kernel.h \
+ /Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/android/log.h \
+ /Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/string.h \
+ /Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/malloc.h
+
+jni/Texture.h:
+
+/Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/jni.h:
+
+jni/SampleUtils.h:
+
+/Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/stdio.h:
+
+/Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/_types.h:
+
+/Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/machine/_types.h:
+
+/Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/types.h:
+
+/Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/stdint.h:
+
+/Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/stddef.h:
+
+/Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/compiler.h:
+
+/Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/asm/types.h:
+
+/Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/linux/types.h:
+
+/Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/machine/kernel.h:
+
+/Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/android/log.h:
+
+/Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/string.h:
+
+/Users/aaalaniz/development/android/android-ndk-r7/platforms/android-14/arch-arm/usr/include/malloc.h:
---
+++ jason/src/srdes/menupp/MenuList.java
@@ -0,0 +1,55 @@
+package srdes.menupp;
+
+import com.qualcomm.QCAR.QCAR;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.qualcomm.QCAR.QCAR;
+
+
+public class MenuList extends ListActivity{
+	
+	private String [] menus;
+	private Intent qcarEngine;
+		
+	protected void onCreate(Bundle savedInstanceState) {
+        DebugLog.LOGD("MenuList::onCreate");
+        super.onCreate(savedInstanceState);
+                
+        // Initialize Menus
+        menus = getResources().getStringArray(R.array.menus);
+        setListAdapter(new ArrayAdapter<String>(MenuList.this, R.layout.menu_list, menus));
+        
+        // Initialize intent for Qcar Engine
+        qcarEngine = new Intent(this, QcarEngine.class);
+	}
+	
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		
+		String menu = menus[position];	//TODO: this will allow other menu configurations to be loaded for just one
+	    startActivity(new Intent(this, QcarEngine.class));
+	}
+	
+    protected void onResume()
+    {
+        DebugLog.LOGD("MenuList::onResume");
+        super.onResume();
+    }
+	protected void onPause() {
+		
+		DebugLog.LOGD("MenuList::onPause");
+		super.onPause();	
+	}
+    
+    
+
+}
