da86007d2263c644811ab406817f600c75f01cba
larrylzhao <larrylzhao@aee4f25e-f0af-d29d-9c76-8bc1b7839efd>
2012-02-16 20:40:09 +0000

    Changed PagerAdapter from textView to LayoutInflator

--- larryz/src/srdes/menupp/UserGuide.java
+++ larryz/src/srdes/menupp/UserGuide.java
@@ -3,11 +3,15 @@ package srdes.menupp;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class UserGuide extends Activity {
@@ -30,82 +34,58 @@ public class UserGuide extends Activity {
     }
     
     private class AwesomePagerAdapter extends PagerAdapter{
-
-                
-                @Override
-                public int getCount() {
-                        return NUM_AWESOME_VIEWS;
+    	
+    		 
+            public int getCount() {
+                return 5;
+            }
+     
+            public Object instantiateItem(View collection, int position) {
+     
+                LayoutInflater inflater = (LayoutInflater) collection.getContext()
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+     
+                int resId = 0;
+                switch (position) {
+                case 0:
+                    resId = R.layout.splash;
+                    break;
+                case 1:
+                    resId = R.layout.splash;
+                    break;
+                case 2:
+                    resId = R.layout.splash;
+                    break;
+                case 3:
+                    resId = R.layout.splash;
+                    break;
+                case 4:
+                    resId = R.layout.splash;
+                    break;
                 }
-
-            /**
-             * Create the page for the given position.  The adapter is responsible
-             * for adding the view to the container given here, although it only
-             * must ensure this is done by the time it returns from
-             * {@link #finishUpdate()}.
-             *
-             * @param container The containing View in which the page will be shown.
-             * @param position The page position to be instantiated.
-             * @return Returns an Object representing the new page.  This does not
-             * need to be a View, but can be some other container of the page.
-             */
-                @Override
-                public Object instantiateItem(View collection, int position) {
-                        TextView tv = new TextView(cxt);
-                        tv.setText("Bonjour PAUG " + position);
-                        tv.setTextColor(Color.WHITE);
-                        tv.setTextSize(30);
-                        
-                        ((ViewPager) collection).addView(tv,0);
-                        
-                        return tv;
-                }
-
-            /**
-             * Remove a page for the given position.  The adapter is responsible
-             * for removing the view from its container, although it only must ensure
-             * this is done by the time it returns from {@link #finishUpdate()}.
-             *
-             * @param container The containing View from which the page will be removed.
-             * @param position The page position to be removed.
-             * @param object The same object that was returned by
-             * {@link #instantiateItem(View, int)}.
-             */
-                @Override
-                public void destroyItem(View collection, int position, Object view) {
-                        ((ViewPager) collection).removeView((TextView) view);
-                }
-
-                
-                
-                @Override
-                public boolean isViewFromObject(View view, Object object) {
-                        return view==((TextView)object);
-                }
-
-                
-            /**
-             * Called when the a change in the shown pages has been completed.  At this
-             * point you must ensure that all of the pages have actually been added or
-             * removed from the container as appropriate.
-             * @param container The containing View which is displaying this adapter's
-             * page views.
-             */
-                @Override
-                public void finishUpdate(View arg0) {}
-                
-
-                @Override
-                public void restoreState(Parcelable arg0, ClassLoader arg1) {}
-
-                @Override
-                public Parcelable saveState() {
-                        return null;
-                }
-
-                @Override
-                public void startUpdate(View arg0) {}
-        
+     
+                View view = inflater.inflate(resId, null);
+     
+                ((ViewPager) collection).addView(view, 0);
+     
+                return view;
+            }
+     
+            @Override
+            public void destroyItem(View arg0, int arg1, Object arg2) {
+                ((ViewPager) arg0).removeView((View) arg2);
+     
+            }
+     
+            @Override
+            public boolean isViewFromObject(View arg0, Object arg1) {
+                return arg0 == ((View) arg1);
+     
+            }
+     
+            @Override
+            public Parcelable saveState() {
+                return null;
+            }
     }
-    
-    
 }
