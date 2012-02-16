2a50e788a2d25d33b1dfa63479c8b5b93dd0603b
larrylzhao <larrylzhao@aee4f25e-f0af-d29d-9c76-8bc1b7839efd>
2012-02-16 19:32:51 +0000

    Added swiping abilities to user guide

--- larryz/src/srdes/menupp/UserGuide.java
+++ larryz/src/srdes/menupp/UserGuide.java
@@ -1,61 +1,111 @@
 package srdes.menupp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.ViewFlipper;
+import android.widget.TextView;
 
-public class UserGuide extends Activity implements OnClickListener {
+public class UserGuide extends Activity {
+    
+        private ViewPager awesomePager;
+        private static int NUM_AWESOME_VIEWS = 20;
+        private Context cxt;
+        private AwesomePagerAdapter awesomeAdapter;
+        
+        /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_guide);
+        cxt = this;
+        
+        awesomeAdapter = new AwesomePagerAdapter();
+        awesomePager = (ViewPager) findViewById(R.id.awesomepager);
+        awesomePager.setAdapter(awesomeAdapter);
+    }
+    
+    private class AwesomePagerAdapter extends PagerAdapter{
 
-	ViewFlipper flippy;
-	Button next, previous;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		DebugLog.LOGD("UserGuide::onCreate");
-		super.onCreate(savedInstanceState);
-		
-		// TODO About US
-		setContentView(R.layout.user_guide);
-		flippy = (ViewFlipper) findViewById(R.id.viewFlipper1);
-		next = (Button) findViewById(R.id.bnext);
-		next.setOnClickListener((OnClickListener) this);
-		previous = (Button) findViewById(R.id.bprevious);
-		previous.setOnClickListener((OnClickListener) this);
-	}
-	
-	@Override
-	protected void onResume() {
-		DebugLog.LOGD("UserGuide::onResume");
-		super.onResume();
-	}
-	
-	@Override
-	protected void onPause() {
-		DebugLog.LOGD("UserGuide::onPause");
-		super.onPause();
-	}
+                
+                @Override
+                public int getCount() {
+                        return NUM_AWESOME_VIEWS;
+                }
 
-	@Override
-	protected void onDestroy() {
-		DebugLog.LOGD("UserGuide::onDestroy");
-		super.onDestroy();
-	}
+            /**
+             * Create the page for the given position.  The adapter is responsible
+             * for adding the view to the container given here, although it only
+             * must ensure this is done by the time it returns from
+             * {@link #finishUpdate()}.
+             *
+             * @param container The containing View in which the page will be shown.
+             * @param position The page position to be instantiated.
+             * @return Returns an Object representing the new page.  This does not
+             * need to be a View, but can be some other container of the page.
+             */
+                @Override
+                public Object instantiateItem(View collection, int position) {
+                        TextView tv = new TextView(cxt);
+                        tv.setText("Bonjour PAUG " + position);
+                        tv.setTextColor(Color.WHITE);
+                        tv.setTextSize(30);
+                        
+                        ((ViewPager) collection).addView(tv,0);
+                        
+                        return tv;
+                }
 
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		
-		switch(v.getId()) {
-		
-		case R.id.bnext:
-			flippy.showNext();
-			break;
-			
-		case R.id.bprevious:
-			flippy.showPrevious();
-			break;
-		}
-	}
+            /**
+             * Remove a page for the given position.  The adapter is responsible
+             * for removing the view from its container, although it only must ensure
+             * this is done by the time it returns from {@link #finishUpdate()}.
+             *
+             * @param container The containing View from which the page will be removed.
+             * @param position The page position to be removed.
+             * @param object The same object that was returned by
+             * {@link #instantiateItem(View, int)}.
+             */
+                @Override
+                public void destroyItem(View collection, int position, Object view) {
+                        ((ViewPager) collection).removeView((TextView) view);
+                }
+
+                
+                
+                @Override
+                public boolean isViewFromObject(View view, Object object) {
+                        return view==((TextView)object);
+                }
+
+                
+            /**
+             * Called when the a change in the shown pages has been completed.  At this
+             * point you must ensure that all of the pages have actually been added or
+             * removed from the container as appropriate.
+             * @param container The containing View which is displaying this adapter's
+             * page views.
+             */
+                @Override
+                public void finishUpdate(View arg0) {}
+                
+
+                @Override
+                public void restoreState(Parcelable arg0, ClassLoader arg1) {}
+
+                @Override
+                public Parcelable saveState() {
+                        return null;
+                }
+
+                @Override
+                public void startUpdate(View arg0) {}
+        
+    }
+    
+    
 }
