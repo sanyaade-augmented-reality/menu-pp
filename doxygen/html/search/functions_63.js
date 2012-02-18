beba8a9fa1c31aa8481db0a05769f1d54a96a0cc
Behnke19@gmail.com <Behnke19@gmail.com@aee4f25e-f0af-d29d-9c76-8bc1b7839efd>
2012-02-18 23:26:29 +0000

    What should be a final update of the doxyfile

--- doxygen/html/search/functions_63.js
+++ doxygen/html/search/functions_63.js
@@ -1,10 +1,7 @@
 var searchData=
 [
   ['checkeglerror',['checkEglError',['../classsrdes_1_1menupp_1_1_q_c_a_r_sample_g_l_view.html#aa2b5e94a873173b2a2256e94e1711f39',1,'srdes::menupp::QCARSampleGLView']]],
-  ['chooseconfig',['chooseConfig',['../classsrdes_1_1menupp_1_1_q_c_a_r_sample_g_l_view_1_1_config_chooser.html#a102c0f944ed09e9cdaa1fb709ddbaa87',1,'srdes::menupp::QCARSampleGLView::ConfigChooser.chooseConfig(EGL10 egl, EGLDisplay display)'],['../classsrdes_1_1menupp_1_1_q_c_a_r_sample_g_l_view_1_1_config_chooser.html#a244fe7eefa8959380360fec4286833f7',1,'srdes::menupp::QCARSampleGLView::ConfigChooser.chooseConfig(EGL10 egl, EGLDisplay display, EGLConfig[] configs)']]],
   ['close',['close',['../classsrdes_1_1menupp_1_1_entree_db_adapter.html#ab21604e068726e3d70e9eb92cb6a9b29',1,'srdes::menupp::EntreeDbAdapter']]],
-  ['configchooser',['ConfigChooser',['../classsrdes_1_1menupp_1_1_q_c_a_r_sample_g_l_view_1_1_config_chooser.html#ad253ee49ccc6fce1d097c7c84c672d2a',1,'srdes::menupp::QCARSampleGLView::ConfigChooser']]],
-  ['createcontext',['createContext',['../classsrdes_1_1menupp_1_1_q_c_a_r_sample_g_l_view_1_1_context_factory.html#a11f26fa26576593ff635bad01c9cf6d8',1,'srdes::menupp::QCARSampleGLView::ContextFactory']]],
   ['createnote',['createNote',['../classsrdes_1_1menupp_1_1_view_review.html#a537b46cf887dc482e872989d62bb1c40',1,'srdes::menupp::ViewReview']]],
   ['createreview',['createReview',['../classsrdes_1_1menupp_1_1_entree_db_adapter.html#a1f618c53ef5e465ece90aed0042d0542',1,'srdes::menupp::EntreeDbAdapter']]]
 ];
